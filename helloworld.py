2746b12ff22c0b18251ff3eb44dfda1670b7d96e
RetawLegnan <pyoky@yahoo.co.kr>
2014-07-24 11:58:16 +0900

    helloworld.py 수정 빠졌던 쌍따옴표 추가, 인터프리터와 인코딩 선언 추가

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,11 @@
-print("hello world!)
-input()
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# helloworld.py
+
+
+def main():
+    print("hello world!")
+    input()
+
+if __name__ == '__main__':
+    main()
